148ec35a51eb36c98500727977928a942044a4ff
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-03-25 16:26:34 +0000

    made default run

--- flake.nix
+++ flake.nix
@@ -61,17 +61,9 @@
         inputs',
         ...
       }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
         packages = {
-          default = inputs'.nixpkgs.legacyPackages.figlet;
+          default = self'.packages.cosmos-devnet;
         };
-      };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
       };
     };
 }
