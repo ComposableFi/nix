4ee19c465481c5e5662f173fc323e0799147d800
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-03-27 10:23:49 +0000

    simulate

--- cosmos/mantis.nix
+++ cosmos/mantis.nix
@@ -1,1 +1,32 @@
-mantis-simulate
+{self, ...}: {
+  perSystem = {
+    config,
+    self',
+    inputs',
+    system,
+    pkgs,
+    runtimeInputs,
+    ...
+  }: {
+    packages = let
+      networks = pkgs.networksLib.networks;
+      sh = pkgs.networksLib.sh;
+    in {
+      mantis-simulate = pkgs.writeShellApplication {
+        name = "mantis-simulate";
+        runtimeInputs = runtimeInputs;
+        text = ''
+          ${sh.export networks.devnet.directories}
+          ${sh.export networks.pica.devnet}
+          ${sh.export networks.devnet.mnemonics}
+          ${builtins.readFile ./cosmos_sdk.sh}
+          ${builtins.readFile ./mantis-simulate.sh}
+        '';
+      };
+    };
+  };
+}
+
+
+
+
